dfbcbfff4f8ecdd8a3d0dc8cd0b8de73490f9e25
Justin Chow <hrll71990@gmail.com>
2021-02-11 21:53:05 -0800

    reverting bc lower performance

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 // const compression = require('compression');
 const path = require('path');
-const { service1, service2, service3 } = require('./config/services');
-const axios = require('axios');
 const router = require('./router');
 
 const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
@@ -11,52 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 // app.use(compression());
 
-//app.use('/bundles', router.bundles);
-app.get('/bundles/services1.js', (req, res) => {
-  axios.get(`${service1.url}/${service1.bundle}`)
-    .then((response) => {
-      res.set('Cache-Control', 'public, max-age=31557600'); // one year
-      res.type('.js').send(response.data);
-    })
-    .catch(error => console.log(error));
-});
-
-app.get('/bundles/723.bundle.js', (req, res) => {
-  axios.get(`${service1.url}/723.bundle.js`)
-    .then((response) => {
-      res.set('Cache-Control', 'public, max-age=31557600'); // one year
-      res.type('.js').send(response.data);
-    })
-    .catch(error => console.log(error));
-});
-
-app.get('/bundles/789.bundle.js', (req, res) => {
-  axios.get(`${service1.url}/789.bundle.js`)
-    .then((response) => {
-      res.set('Cache-Control', 'public, max-age=31557600'); // one year
-      res.type('.js').send(response.data);
-    })
-    .catch(error => console.log(error));
-});
-
-app.get('/bundles/services2.js', (req, res) => {
-  axios.get(`${service2.url}/${service2.bundle}`)
-    .then((response) => {
-      res.set('Cache-Control', 'public, max-age=31557600'); // one year
-      res.type('.js').send(response.data);
-    })
-    .catch(error => console.log(error));
-});
-
-app.get('/bundles/services3.js', (req, res) => {
-  axios.get(`${service3.url}/${service3.bundle}`)
-    .then((response) => {
-      res.set('Cache-Control', 'public, max-age=31557600'); // one year
-      res.type('.js').send(response.data);
-    })
-    .catch(error => console.log(error));
-});
-
+app.use('/bundles', router.bundles);
 app.use('/api', router.api);
 
 app.use(express.static(PUBLIC_DIR));
